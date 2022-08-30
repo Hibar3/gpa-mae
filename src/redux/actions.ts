0b393c7b2e5799aaa1a29b31fcbb104b7293048c
Hibar3 <jongxiankhoo@gmail.com>
2022-08-30 23:21:04 +0800

    feat: update actions

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -1,42 +1,29 @@
 import * as types from "./types";
 
-export const fetchPlaces = (searchTerm) => {
-  console.log("fetchPlaces", searchTerm);
-  return { type: types.FETCH_PLACES, payload: { searchTerm } };
-};
-export const fetchPlacesSuccess = (searchTerm) => {
-  console.log("fetchPlacesSuccess", searchTerm);
-  return {
-    type: types.FETCH_PLACES_SUCCESS,
-    payload: { searchTerm },
-  };
-};
+export const fetchPlaces = (searchTerm) => ({
+  type: types.FETCH_PLACES,
+  payload: { searchTerm },
+});
+export const fetchPlacesSuccess = (searchTerm) => ({
+  type: types.FETCH_PLACES_SUCCESS,
+  payload: { searchTerm },
+});
 
 export const fetchPlacesFailure = (error, places) => ({
   type: types.FETCH_PLACES_FAILURE,
   payload: { error, places },
 });
 
-export const fetchGeo = (address) => ({
+export const fetchGeo = (searchTerm) => ({
   type: types.FETCH_GEO,
-  payload: { address },
+  payload: { searchTerm },
 });
-export const fetchGeoSuccess = (GEO) => ({
+export const fetchGeoSuccess = (searchTerm) => ({
   type: types.FETCH_GEO_SUCCESS,
-  payload: { GEO },
+  payload: { searchTerm },
 });
 
 export const fetchGeoFailure = (error, places) => ({
   type: types.FETCH_GEO_FAILURE,
   payload: { error, places },
 });
-
-// export function updateSearches(data) {
-//   console.log({ data });
-//   return (dispatch) => {
-//     dispatch({
-//       type: 'UPDATE_SEARCHES',
-//       payload: data,
-//     });
-//   };
-// }
