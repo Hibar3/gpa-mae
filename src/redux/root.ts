e20c3556db8984ecbb26a027fd40d231648626a9
Hibar3 <jongxiankhoo@gmail.com>
2022-08-30 23:55:58 +0800

    feat: update root store

--- src/redux/root.ts
+++ src/redux/root.ts
@@ -1,31 +1,14 @@
-// @ts-nocheck - may need to be at the start of file
 import { combineEpics, ofType } from "redux-observable";
-import { ajax } from "rxjs/ajax";
-import { of } from "rxjs";
-import {
-  takeUntil,
-  mergeMap,
-  catchError,
-  map,
-  tap,
-  filter,
-  mapTo,
-  delay,
-} from "rxjs/operators";
-import * as actions from "./actions";
+import { mergeMap, tap } from "rxjs/operators";
 import * as types from "./types";
-import { apiEndpoint, apiKey } from "../config";
+import { handleAddress } from "../utils";
 
 // epic
 export const fetchPlacesEpic = (action$, state) => {
-  console.log("action:", action$);
   return action$.pipe(
-    ofType(types.FETCH_PLACES),
+    ofType(types.FETCH_GEO_SUCCESS),
     tap((value) => console.log("Gonna fetch", value)),
-    mergeMap((value) => {
-      console.log("line 58", value);
-      return value.payload;
-    })
+    mergeMap(async ({ payload }) => handleAddress(payload?.searchTerm?.placeId))
   );
 };
 
